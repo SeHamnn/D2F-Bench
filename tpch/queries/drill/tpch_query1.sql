f6cf7285cd0ef4b5ef3dcb78d4658ac904c335f5
SeHamnn <hamann.sebastian@gmx.de>
2017-05-24 12:53:17 +0200

    Update tpch_query1.sql

--- tpch/queries/drill/tpch_query1.sql
+++ tpch/queries/drill/tpch_query1.sql
@@ -1,25 +1,22 @@
 use DATABASE;
 select
-	n_name,
-	sum(l_extendedprice * (1 - l_discount)) as revenue
+	l_returnflag,
+	l_linestatus,
+	sum(l_quantity) as sum_qty,
+	sum(l_extendedprice) as sum_base_price,
+	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+	avg(l_quantity) as avg_qty,
+	avg(l_extendedprice) as avg_price,
+	avg(l_discount) as avg_disc,
+	count(*) as count_order
 from
-	customer,
-	orders,
-	lineitem,
-	supplier,
-	nation,
-	region
+	lineitem
 where
-	c_custkey = o_custkey
-	and l_orderkey = o_orderkey
-	and l_suppkey = s_suppkey
-	and c_nationkey = s_nationkey
-	and s_nationkey = n_nationkey
-	and n_regionkey = r_regionkey
-	and r_name = 'AFRICA'
-	and o_orderdate >= '1993-01-01'
-	and o_orderdate < '1994-01-01'
+	l_shipdate <= '1998-09-16'
 group by
-	n_name
+	l_returnflag,
+	l_linestatus
 order by
-	revenue desc;
+	l_returnflag,
+	l_linestatus;
